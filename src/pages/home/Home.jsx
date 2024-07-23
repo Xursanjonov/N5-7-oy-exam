fefab856c986250e9f6a2511c2e37d6d24653cf8
Xursanjonov <nxursanjonov888@gmail.com>
2024-07-24 01:29:00 +0500

    add to home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,8 @@
 import React, { memo } from 'react'
 import Hero from '../../components/hero'
 import Sale from '../../components/sale'
+import Newsletter from '../../components/newsletter'
+import ProductsCart from '../../components/products-cart'
 import './home.scss'
 
 const Home = () => {
@@ -8,7 +10,22 @@ const Home = () => {
     return (
         <section className='home'>
             <Hero />
+            <div className='home__products container__ns'>
+                <ProductsCart />
+                <ProductsCart />
+                <ProductsCart />
+                <ProductsCart />
+                <ProductsCart />
+                <ProductsCart />
+                <ProductsCart />
+                <ProductsCart />
+                <ProductsCart />
+                <ProductsCart />
+                <ProductsCart />
+            </div>
             <Sale />
+            <div className='articales'></div>
+            <Newsletter />
         </section>
     )
 }
