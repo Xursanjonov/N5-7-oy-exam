716440a43f5e72b311a5d615069c080137e9c11c
Xursanjonov <nxursanjonov888@gmail.com>
2024-07-25 22:58:52 +0500

    add to like

--- src/pages/shop/index.jsx
+++ src/pages/shop/index.jsx
@@ -1,8 +1,25 @@
-import React, { memo } from 'react'
+import React, { memo, useState } from 'react'
+import { useGetProductsQuery } from '../../context/api/productApi'
+import ProductsItem from '../../components/products-cart/ProductsItem'
+import Loading from '../../components/loading'
+import './shop.scss'
 
 const Shop = () => {
-    return (
-        <section>Shop</section>
+    const [limit, setLimit] = useState(8)
+    const [page, setPage] = useState(1)
+    const { data, isFetching } = useGetProductsQuery({ limit: limit, page: 1 })
+
+    return isFetching ? <Loading /> : (
+        <section className='shop'>
+            <div className='shop__container'>
+                {
+                    data?.map(el => (
+                        <ProductsItem key={el?.id} product={el} />
+                    ))
+                }
+                <button onClick={() => setLimit(p => p + 4)} className="btn-more">Show more</button>
+            </div>
+        </section>
     )
 }
 
--- src/pages/wish-list/index.jsx
+++ src/pages/wish-list/index.jsx
@@ -1,15 +1,17 @@
 import React, { memo, useEffect } from 'react'
 import { useSelector } from 'react-redux'
-import Loadinig from '../../components/loading'
 import ProductsItem from '../../components/products-cart/ProductsItem'
+import emptyImage from '../../assets/images/wish-list.jpg'
 import './wish-list.scss'
+import { useNavigate } from 'react-router-dom'
 
 const WishList = () => {
     const data = useSelector(state => state.wishlist.value)
+    const navigate = useNavigate()
 
     useEffect(() => { window.scroll(0, 0) }, [])
 
-    return data ? (
+    return data?.length ? (
         <section className='wish-list'>
             <div className="wish-list__container">
                 <h2 className='like__title'>WishList</h2>
@@ -21,7 +23,15 @@ const WishList = () => {
             </div>
         </section>
 
-    ) : <Loadinig />
+    ) : <section className='wish-list__empty'>
+        <figure className='wish-list__empty-img'>
+            <img className='empty-img' src={emptyImage} alt="" />
+        </figure>
+        <div className="wish-list__btns">
+            <button className='back' onClick={() => navigate(-1)}>Go Back</button>
+            <button className='go-products' onClick={() => navigate('/shop')}>Go Products</button>
+        </div>
+    </section>
 }
 
 export default memo(WishList)
